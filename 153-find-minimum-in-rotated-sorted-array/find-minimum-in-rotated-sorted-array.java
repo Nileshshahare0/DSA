d7e86fb5591871a4611ecdb4282b206a371c842d
Nilesh Shahare <nileshshahare008@gmail.com>
2026-07-26 23:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (99.68%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,41 +1,11 @@
 class Solution {
     public int findMin(int[] nums) {
-        sorteds(nums , 0 , nums.length-1);
-        return nums[0];
-    }
-    public  void sorteds(int[]arr , int si , int ei){
-        if(si>=ei){
-            return;
-        }
-        int mid = si + (ei-si)/2;
-        sorteds(arr , si , mid);
-        sorteds(arr , mid+1 , ei);
-        merged(arr , mid , si , ei);
-    }
-    public void merged(int[]arr ,int mid , int si ,int ei){
-        int[]temp = new int[ei - si + 1];
-        int i = si;
-        int j = mid +1;
-        int k = 0;
-        while(i <= mid && j<=ei){
-            if(arr[i] <= arr[j]){
-                temp[k] = arr[i];
-                i++;
-            }else{
-                temp[k] = arr[j];
-                j++;
+        int min = nums[0];
+        for(int i = 0 ; i<nums.length ; i++){
+            if(min>nums[i]){
+                min = nums[i];
             }
-            k++;
         }
-        while(i<=mid){
-            temp[k++] = arr[i++];
-        }
-        while(j<=ei){
-            temp[k++] = arr[j++];
-        }
-        i = si;
-        for(int n = 0 ; n<temp.length ; n++){
-            arr[i++] = temp[n];
-        }
+        return min;
     }
 }
